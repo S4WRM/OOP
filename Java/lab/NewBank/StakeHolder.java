c0f8625dea8811d627b5380103368c4abfce40a0
S4WRM <161490868+S4WRM@users.noreply.github.com>
2024-03-20 13:48:43 -0500

    Update and rename Java/lab/Bank.java to Java/lab/NewBank/StakeHolder.java

---
+++ Java/lab/NewBank/StakeHolder.java
@@ -0,0 +1,63 @@
+package NewBank;
+
+public class StakeHolder {
+    
+    private String name;
+    private int nuip;
+    private String address;
+    private String mail;
+    private double money;
+
+    public StakeHolder(String name, int nuip, String address, String mail, double money) {
+        this.name = name;
+        this.nuip = nuip;
+        this.address = address;
+        this.mail = mail;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNuip() {
+        return nuip;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+             
+   
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+
