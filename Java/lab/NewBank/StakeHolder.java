71a167f67ae031f23477014d7a7a3748c764d264
S4WRM <161490868+S4WRM@users.noreply.github.com>
2024-03-21 07:46:40 -0500

    Update StakeHolder.java

--- Java/lab/NewBank/StakeHolder.java
+++ Java/lab/NewBank/StakeHolder.java
@@ -55,9 +55,39 @@ public class StakeHolder {
    
     
     public static void main(String[] args) {
+
+         List<Integer> funds = new ArrayList<Integer>();
+        funds.add(100);
+        funds.add(900);
+        funds.add(300);
+        funds.add(800);
+        funds.add(90);
+        
+       // funds.add(0, 750);
+        for (Integer fund : funds) {
+           System.out.println(fund);
+        }
+        
+        System.out.println("*************"); 
+       funds.remove(2);
+       //lista actualizada 
+        for (Integer fund : funds) {
+           System.out.println(fund);
+        }
+        
+        int suma=0;
+         for (Integer fund : funds) {
+             suma=suma+fund;
+             
+            System.out.println("los fondos son de:" +suma);
+             
+         }
+  
+    }
+        
         
     }
     
-}
+
 
 
